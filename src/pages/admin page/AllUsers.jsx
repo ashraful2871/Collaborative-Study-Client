3671f198a78ade6dfc9ad1f25f4e1ad5d5f4d48f
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-15 00:58:34 +0600

    fetch all user

--- src/pages/admin page/AllUsers.jsx
+++ src/pages/admin page/AllUsers.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
 
 const AllUsers = () => {
+  const axiosSecure = useAxiosSecure();
+  const { data: users } = useQuery({
+    queryKey: ["users"],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get("/users");
+      return data;
+    },
+  });
+  console.log(users);
   return (
     <div>
-      <h2>all user</h2>
+      <h2>all user: {users.length}</h2>
     </div>
   );
 };
