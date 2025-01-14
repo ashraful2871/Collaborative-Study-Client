12b6ffb4c16e0a1487a388d55941ede19972f19b
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-14 14:26:49 +0600

    add banner section iin home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Slider from "./slider/Slider";
 
 const Home = () => {
   return (
     <div>
       <h2>i am home</h2>
+      <Slider></Slider>
     </div>
   );
 };
