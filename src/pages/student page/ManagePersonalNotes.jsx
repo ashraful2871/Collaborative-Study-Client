9866403aa8d869317c250c42b2a1e7f696be4d9b
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-16 09:54:13 +0600

    fetch all create note and show ui

--- src/pages/student page/ManagePersonalNotes.jsx
+++ src/pages/student page/ManagePersonalNotes.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import ManagePersonalNoteCart from "./ManagePersonalNoteCart";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../../components/Loading";
 
 const ManagePersonalNotes = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+  const { data: notes = [], isLoading } = useQuery({
+    queryKey: ["note", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/student/note/${user?.email}`);
+      return data;
+    },
+  });
+  console.log(notes);
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
       <h2>i am ManagePersonalNotes</h2>
+      <div className="grid grid-cols-4 gap-5">
+        {notes.map((note) => (
+          <ManagePersonalNoteCart
+            key={note._id}
+            note={note}
+          ></ManagePersonalNoteCart>
+        ))}
+      </div>
     </div>
   );
 };
