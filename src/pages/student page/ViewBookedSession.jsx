09b991e6042a1285f0d8c9e103a3b7cd0c3132be
ashraful2871 <ashrafulislam2758@gmail.com>
2025-01-16 16:42:53 +0600

    add view booked session data and show in specific user who booked

--- src/pages/student page/ViewBookedSession.jsx
+++ src/pages/student page/ViewBookedSession.jsx
@@ -1,9 +1,36 @@
 import React from "react";
+import useAuth from "../../hooks/useAuth";
+import useAxiosSecure from "../../hooks/useAxiosSecure";
+import { useQuery } from "@tanstack/react-query";
+import ViewBookedSessionCard from "./ViewBookedSessionCard";
+import Loading from "../../components/Loading";
 
 const ViewBookedSession = () => {
+  const { user } = useAuth();
+  const axiosSecure = useAxiosSecure();
+
+  const { data: bookedSessions = [], isLoading } = useQuery({
+    queryKey: ["book-session", user?.email],
+    queryFn: async () => {
+      const { data } = await axiosSecure.get(`/book-session/${user?.email}`);
+      return data;
+    },
+  });
+  console.log(bookedSessions);
+  if (isLoading) {
+    return <Loading></Loading>;
+  }
   return (
     <div>
       <h2>i am ViewBookedSession</h2>
+      <div className="grid grid-cols-3 gap-5">
+        {bookedSessions.map((session) => (
+          <ViewBookedSessionCard
+            key={session._id}
+            session={session}
+          ></ViewBookedSessionCard>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/pages/student page/ViewBookedSessionCard.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+const ViewBookedSessionCard = ({ session }) => {
+  const { image, sessionTitle, description } = session;
+  return (
+    <div className="card bg-base-100 shadow-md w-full max-w-lg mx-auto flex flex-col">
+      <figure className="h-64 w-full">
+        <img src={image} alt="Course" className="w-full h-full object-cover" />
+      </figure>
+
+      <div className="card-body p-5 space-y-3 flex flex-col flex-grow">
+        <div className="flex justify-between items-center">
+          <span className="text-sm text-gray-500">Tutor</span>
+          <span className="text-lg font-semibold text-blue-500">$100</span>
+        </div>
+
+        <h2 className="card-title text-xl font-bold">{sessionTitle}</h2>
+
+        <p className="text-sm text-gray-600 flex-grow line-clamp-3">
+          {description}
+        </p>
+
+        <div className="card-actions">
+          <button className="btn btn-outline btn-primary">Read More</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ViewBookedSessionCard;
