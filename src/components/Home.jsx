817b3f6aeb3b7bfb8af1ff7b64594d07ac601339
ashraful2871 <ashrafulislam2758@gmail.com>
2025-02-18 16:59:02 +0600

    add popular teacher

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Slider from "./slider/Slider";
 import StudySession from "./studey session section/StudySession";
 import AllTutor from "./tutor/AllTutor";
+import PopularTutor from "./tutor/PopularTutor";
 
 const Home = () => {
   return (
     <div className="space-y-12 mb-12">
       <Slider></Slider>
       <StudySession></StudySession>
+      <PopularTutor></PopularTutor>
       <AllTutor></AllTutor>
     </div>
   );
