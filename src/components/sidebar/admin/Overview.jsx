0c37e3db5ea4e540ee46d87e7b6d05e975cc5f84
ashraful2871 <ashrafulislam2758@gmail.com>
2025-05-04 09:41:55 +0600

    add new comment line in admin dashboard pagge for thee code batter radibilty

--- src/components/sidebar/admin/Overview.jsx
+++ src/components/sidebar/admin/Overview.jsx
@@ -7,9 +7,11 @@ const Overview = () => {
     <div>
       <h2 className="text-3xl  font-bold mb-5 text-center">Overview</h2>
 
+      {/* weekly seals chart */}
       <div>
         <WeeklyChart></WeeklyChart>
       </div>
+      {/* best selling products in website */}
       <div>
         <BestSellingProductsChart></BestSellingProductsChart>
       </div>
